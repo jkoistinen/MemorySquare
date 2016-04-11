34db242a1c0b1ce6da1373da5b71c780a1e54776
jk <jk>
2016-04-11 15:06:05 +0200

    animation when a square is clicked

--- app/src/main/java/com/jk/memorysquare/PlayGameActivity.java
+++ app/src/main/java/com/jk/memorysquare/PlayGameActivity.java
@@ -1,7 +1,12 @@
 package com.jk.memorysquare;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PlayGameActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class PlayGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+
+        //set onclicklistener for imagebuttons
+        ImageButton redButton = (ImageButton) findViewById(R.id.redButton);
+
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Red", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/jk/memorysquare/StartGameActivity.java
+++ app/src/main/java/com/jk/memorysquare/StartGameActivity.java
@@ -1,11 +1,13 @@
 package com.jk.memorysquare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class StartGameActivity extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class StartGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_game);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Start game button launches PlayGameActivity
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PlayGameActivity.class);
+                startActivity(i);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
